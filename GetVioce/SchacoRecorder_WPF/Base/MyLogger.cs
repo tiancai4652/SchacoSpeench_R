aedf2f49486c499c4458371ac7859b7adc159ca7
Shaco <zhangranmm@126.com>
2019-04-24 18:57:13 +0800

    wpf项目增加日志框架

---
+++ GetVioce/SchacoRecorder_WPF/Base/MyLogger.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchacoRecorder_WPF
+{
+    public class MyLogger
+    {
+        static ILogger _LoggerInstance;
+        public static ILogger LoggerInstance
+        {
+            get
+            {
+                if (_LoggerInstance == null)
+                {
+                    _LoggerInstance = LogManager.GetCurrentClassLogger();
+                }
+                return _LoggerInstance;
+            }
+        }
+    }
+}
